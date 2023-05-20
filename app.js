dc3f192788afb2a222aca1c045ade4ab84593355
moknine <mokninemoknine999@gmail.com>
2023-05-19 22:42:59 -1200

    crreate landing page

---
+++ app.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multi-text", {
+    strings: ["Freelancer", "Frontend Developer", "YouTuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    bckDelay: 1000,
+    loop: true
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+"# create landing profile version kendric"
